91db6f1d6bef3d4f2109ce374febd2428d1025bc
lost-dream <13623673118@163.com>
2022-06-27 13:30:05 +0800

    feat: 修改 vue-router 引用

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import './design/tailwind.css'
+import './design/tailwind.css';
 
-createApp(App).use(router).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import { router, setupRouter } from './router';
+
+async function bootstrap() {
+  const app = createApp(App);
+
+  await setupRouter(app);
+
+  await router.isReady();
+  app.mount('#app');
+}
+
+bootstrap();
